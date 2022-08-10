d0df994d7c9dd2fc093764ee4213d2832a826372
luodong <luod1027@gmail.com>
2022-08-10 20:35:59 +0800

    remove big int

--- gen.cpp
+++ gen.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main(int argc, char **argv)
 {
     string filename(argv[1]);
-    int64_t size = stoi(string(argv[2]));
+    int64_t size = strtoll(argv[2], nullptr, 10);
 
     ofstream lif;
     lif.open(filename, ios::trunc);
@@ -18,17 +18,24 @@ int main(int argc, char **argv)
     }
     srand(time(0));
 
-    int cnt = 0;
+    double step = 0;
+    int64_t cnt = 0;
     lif << "#Life 1.06\n";
     for (int64_t i = -size; i <= size; i++)
     {
         for (int64_t j = -size; j <= size; j++)
         {
-            if (rand() % 2 == 1)
+            if (rand() % 50 == 1)
             {
                 lif << i << " " << j << "\n";
                 ++cnt;
             }
+        }
+
+        if ((i / (double)size + 1 - step) >= 0.1)
+        {
+            step += 0.1;
+            cout << step << endl;
         }
     }
     lif.flush();
